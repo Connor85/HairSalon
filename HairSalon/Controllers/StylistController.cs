852637adfa058a939e7f53b2a06bd609f91a35e1
cm <connormccarthy8@gmail.com>
2018-09-21 10:43:35 -0700

    add create form for stylists. add stylist page.

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistController : Controller
+    {
+        [HttpGet("/stylists")]
+        public ActionResult Index()
+        {
+            List<Stylist> allStylists = Stylist.GetAll();
+            return View(allStylists);
+        }
+        
+        [HttpGet("/stylists/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+    }
+}
