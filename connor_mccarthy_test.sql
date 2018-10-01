0f2dad244bc05b0b45a3af9796b532d6029abcb6
cm <connormccarthy8@gmail.com>
2018-10-01 12:44:35 -0700

    add database files

--- connor_mccarthy_test.sql
+++ connor_mccarthy_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 01, 2018 at 07:42 PM
+-- Generation Time: Oct 01, 2018 at 07:43 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `connor_mccarthy`
+-- Database: `connor_mccarthy_test`
 --
 
 -- --------------------------------------------------------
@@ -32,19 +32,6 @@ CREATE TABLE `clients` (
   `stylist_id` int(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `clients`
---
-
-INSERT INTO `clients` (`id`, `name`, `stylist_id`) VALUES
-(18, 'Mitch', 8),
-(19, 'connor', 0),
-(20, 'connor', 0),
-(21, 'connor', 0),
-(22, 'connor', 0),
-(23, 'connor', 0),
-(24, 'connor', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -55,15 +42,6 @@ CREATE TABLE `specialties` (
   `id` int(11) NOT NULL,
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `specialties`
---
-
-INSERT INTO `specialties` (`id`, `description`) VALUES
-(1, 'hair coloring'),
-(5, 'Long Hair'),
-(6, 'hair coloring');
 
 -- --------------------------------------------------------
 
@@ -77,13 +55,6 @@ CREATE TABLE `specialties_stylists` (
   `stylist_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `specialties_stylists`
---
-
-INSERT INTO `specialties_stylists` (`id`, `specialty_id`, `stylist_id`) VALUES
-(19, 1, 8);
-
 -- --------------------------------------------------------
 
 --
@@ -95,14 +66,6 @@ CREATE TABLE `stylists` (
   `name` varchar(255) NOT NULL,
   `hire_date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `stylists`
---
-
-INSERT INTO `stylists` (`id`, `name`, `hire_date`) VALUES
-(8, 'Chan', '2018-09-29'),
-(9, 'Jeff', '2018-09-30');
 
 -- --------------------------------------------------------
 
